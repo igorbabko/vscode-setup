2101bd12d606b10a284359bc6762e99ec182773e
Igor Babko <i.i.babko@gmail.com>
2025-09-01 18:37:33 +0300

    remove file utils

--- init.lua
+++ init.lua
@@ -45,17 +45,6 @@ vim.opt.smartcase = true
 
 
 if vim.g.vscode then
-  -- File Utils Extension
-  vim.keymap.set('n', '<leader>u,', function() vim.fn.VSCodeNotify('fileutils.newFile') end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<leader>u.', function() vim.fn.VSCodeNotify('fileutils.newFolder') end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<leader>um', function() vim.fn.VSCodeNotify('fileutils.newFileAtRoot') end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<leader>u/', function() vim.fn.VSCodeNotify('fileutils.newFolderAtRoot') end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<leader>uc', function() vim.fn.VSCodeNotify('fileutils.duplicateFile') end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<leader>ud', function() vim.fn.VSCodeNotify('fileutils.removeFile') end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<leader>uu', function() vim.fn.VSCodeNotify('fileutils.moveFile') end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<leader>ur', function() vim.fn.VSCodeNotify('fileutils.renameFile') end, { noremap = true, silent = true })
-  vim.keymap.set('n', '<leader>un', function() vim.fn.VSCodeNotify('fileutils.copyFileName') end, { noremap = true, silent = true })
-
   -- Respect camelCase and snake_case word motions
   vim.keymap.set('n', 'w', function() vim.fn.VSCodeNotify('cursorWordPartRight') end, { noremap = true, silent = true })
   vim.keymap.set('n', 'b', function() vim.fn.VSCodeNotify('cursorWordPartLeft') end, { noremap = true, silent = true })
