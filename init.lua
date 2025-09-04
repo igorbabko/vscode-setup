2c12166c170654649f22fe17c8cc41eb70dc7721
Igor Babko <i.i.babko@gmail.com>
2025-09-04 10:00:27 +0300

    07. Window management

--- init.lua
+++ init.lua
@@ -43,11 +43,13 @@ if vim.g.vscode then
   local opts = { noremap = true, silent = true }
 
   local mappings = {
+    -- Word Navigation
     { 'n', 'w', 'cursorWordPartRight' },
     { 'n', 'b', 'cursorWordPartLeft' },
     { 'v', 'w', 'cursorWordPartRightSelect' },
     { 'v', 'b', 'cursorWordPartLeftSelect' },
 
+    -- Code Navigation
     { 'n', '<leader>gd', 'editor.action.revealDefinition' },
     { 'n', '<leader>gy', 'editor.action.goToTypeDefinition' },
     { 'n', '<leader>gi', 'editor.action.goToImplementation' },
@@ -58,6 +60,7 @@ if vim.g.vscode then
     { 'n', '<leader>nb', 'workbench.action.navigateBack' },
     { 'n', '<leader>je', 'workbench.action.navigateToLastEditLocation' },
 
+    -- Quick Search & Peek Actions
     { 'n', '<leader>ss', 'workbench.action.showAllSymbols' },
     { 'n', '<leader>sa', 'workbench.action.showCommands' },
     { 'n', '<leader>sf', 'workbench.action.quickOpen' },
@@ -68,6 +71,7 @@ if vim.g.vscode then
     { 'n', '<leader>fr', 'references-view.findReferences' },
     { 'n', '<leader>sr', 'editor.action.referenceSearch.trigger' },
 
+    -- Find, Replace & Refactoring
     { 'n', '<leader>ff', 'actions.find' },
     { 'n', '<leader>fr', 'editor.action.startFindReplaceAction' },
     { 'n', '<leader>fg', 'workbench.action.findInFiles' },
@@ -77,6 +81,7 @@ if vim.g.vscode then
     { 'n', '<leader>qf', 'editor.action.quickFix' },
     { 'n', '<leader>sg', 'editor.action.triggerSuggest' },
 
+    -- Line Editing & Code Maintenance
     { 'n', '<leader>lu', 'editor.action.copyLinesUpAction' },
     { 'n', '<leader>ld', 'editor.action.copyLinesDownAction' },
     { 'n', '<leader>mu', 'editor.action.moveLinesUpAction' },
@@ -86,6 +91,7 @@ if vim.g.vscode then
     { 'n', '<leader>en', 'editor.action.marker.next' },
     { 'n', '<leader>ep', 'editor.action.marker.prev' },
 
+    -- File & Workspace Management
     { 'n', '<leader>cp', 'copyFilePath' },
     { 'n', '<leader>cr', 'copyRelativeFilePath' },
     { 'n', '<leader>rl', 'workbench.action.openRecent' },
@@ -95,6 +101,12 @@ if vim.g.vscode then
     { 'n', '<leader>of', 'workbench.action.files.openFile' },
     { 'n', '<leader>rw', 'workbench.action.reloadWindow' },
     { 'n', '<leader>os', 'workbench.action.openSettingsJson' },
+
+    -- Window Management
+    { 'n', '<leader>wv', 'workbench.action.splitEditor' },
+    { 'n', '<leader>wh', 'workbench.action.splitEditorDown' },
+    { 'n', '<leader>ww', 'workbench.action.joinTwoGroups' },
+    { 'n', '<leader>wa', 'workbench.action.evenEditorWidths' },
   }
 
   for _, mapping in ipairs(mappings) do
