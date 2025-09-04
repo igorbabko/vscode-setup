422669de72892bc3616dc4e2ba8a3b441b4c2474
Igor Babko <i.i.babko@gmail.com>
2025-09-04 09:48:50 +0300

    06. File & workspace management

--- init.lua
+++ init.lua
@@ -85,6 +85,16 @@ if vim.g.vscode then
     { 'n', '<leader>oi', 'editor.action.organizeImports' },
     { 'n', '<leader>en', 'editor.action.marker.next' },
     { 'n', '<leader>ep', 'editor.action.marker.prev' },
+
+    { 'n', '<leader>cp', 'copyFilePath' },
+    { 'n', '<leader>cr', 'copyRelativeFilePath' },
+    { 'n', '<leader>rl', 'workbench.action.openRecent' },
+    { 'n', '<leader>nf', 'workbench.action.files.newUntitledFile' },
+    { 'n', '<leader>cf', 'workbench.action.closeActiveEditor' },
+    { 'n', '<leader>fa', 'workbench.action.closeAllEditors' },
+    { 'n', '<leader>of', 'workbench.action.files.openFile' },
+    { 'n', '<leader>rw', 'workbench.action.reloadWindow' },
+    { 'n', '<leader>os', 'workbench.action.openSettingsJson' },
   }
 
   for _, mapping in ipairs(mappings) do
