de1cfbe1df5683e5dd13c191e080dc584a939bce
Igor Babko <i.i.babko@gmail.com>
2025-10-24 12:49:45 +0300

    05. Repeats

--- init.lua
+++ init.lua
@@ -124,6 +124,11 @@ require("lazy").setup({
             },
           },
         })
+
+        -- Make movements repeatable with ' and ,
+        local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+        vim.keymap.set({ "n", "x", "o" }, "'", ts_repeat_move.repeat_last_move_next)
+        vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
       end
     },
   },
